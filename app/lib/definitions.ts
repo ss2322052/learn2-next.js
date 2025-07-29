9c03afa1e13a268fe46f32158301475f555fb87a
早川ふたば <s2322052@stu.musashino-u.ac.jp>
2025-07-29 19:06:42 +0900

    Usersページ作成

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -1,13 +1,9 @@
-// This file contains type definitions for your data.
-// It describes the shape of the data, and what data type each property should accept.
-// For simplicity of teaching, we're manually defining these types.
-// However, these types are generated automatically if you're using an ORM such as Prisma.
 export type User = {
   id: string;
   name: string;
   email: string;
   password: string;
-  role: 'admin' | 'guest'; // ✨ 追加: ユーザーの役割
+  role: 'admin' | 'user'; // ユーザの役割を定義する。管理者と一般ユーザの2つの役割がある。
 };
 
 export type Customer = {
@@ -40,7 +36,6 @@ export type LatestInvoice = {
   amount: string;
 };
 
-// The database returns a number for amount, but we later format it to a string with the formatCurrency function
 export type LatestInvoiceRaw = Omit<LatestInvoice, 'amount'> & {
   amount: number;
 };
--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,10 +1,15 @@
+// app/ui/dashboard/sidenav.tsx
+
 import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
-import AcmeLogo from '@/app/ui/acme-logo'; 
+import AcmeLogo from '@/app/ui/acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
-import { signOut } from '@/auth';
+import { auth, signOut } from '@/auth';
 
-export default function SideNav() {
+export default async function SideNav() {
+  const session = await auth(); 
+  const userRole = session?.user?.role || 'user'; 
+
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
@@ -16,7 +21,7 @@ export default function SideNav() {
         </div>
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
-        <NavLinks />
+        <NavLinks userRole={userRole} /> 
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
         <form
           action={async () => {
@@ -24,7 +29,7 @@ export default function SideNav() {
             await signOut();
           }}
         >
-          <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
             <PowerIcon className="w-6" />
             <div className="hidden md:block">Sign Out</div>
           </button>
